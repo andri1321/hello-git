6ae128ce47e56601737cd32e97f975920b6f6884
Andri <Andriking320@gmail.com>
2024-01-19 15:44:34 -0400

    ya la 2 classes

--- holamundof.dart
+++ holamundof.dart
@@ -9,4 +9,13 @@ void main() {
     'Alfredo'
   ];
   print(nombres[4]);
+
+  String semanas = semana(text: 'dia de las semanas', dia: '7');
+  print( semanas );
 }
+
+String semana({required String text, required String dia}){
+  return '$text, $dia';
+}
+
+
