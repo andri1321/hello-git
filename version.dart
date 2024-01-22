c62cf1a2476dcdd11c08fde66931f744900c228a
Andri <Andriking320@gmail.com>
2024-01-22 13:01:45 -0400

    nueva version

---
+++ version.dart
@@ -0,0 +1,17 @@
+
+
+
+
+class Casa {
+  final int numero; 
+ final String color;
+ final String ubicacion;
+ final String pais;
+
+  Casa({
+    required this.numero, 
+    required this.color, 
+    required this.ubicacion, 
+    required this.pais
+    });
+}
